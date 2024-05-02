431ffa86e15c658c65071345195e8a468bc169a5
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-05-02 17:18:32 +0300

    added couple react hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,12 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting(props) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
-
+  const [name, setName] = React.useState(props.initialName)
+  console.log(props)
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +23,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Dima" />
 }
 
 export default App
